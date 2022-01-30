28a1de6e475acf370b4f6a66cc0082e7be2e5823
Sai Kumar <sai@vitwit.com>
2022-01-30 15:29:13 +0530

    chore: add claim module account on initGenesis time

--- app/app.go
+++ app/app.go
@@ -144,7 +144,7 @@ var (
 		stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
 		govtypes.ModuleName:            {authtypes.Burner},
 		ibctransfertypes.ModuleName:    {authtypes.Minter, authtypes.Burner},
-		claimtypes.ModuleName:          {authtypes.Minter, authtypes.Burner},
+		claimtypes.ModuleName:          nil,
 	}
 )
 
--- x/claim/genesis.go
+++ x/claim/genesis.go
@@ -10,10 +10,12 @@ import (
 
 // InitGenesis initializes the claim module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	// set up the module account with balance
+	k.CreateModuleAccount(ctx, genState.ModuleAccountBalance)
+
 	// If it's the chain genesis, set the airdrop start time to be now, and set up the needed module accounts.
 	if genState.Params.AirdropStartTime.Equal(time.Time{}) {
 		genState.Params.AirdropStartTime = ctx.BlockTime()
-		k.CreateModuleAccount(ctx, genState.ModuleAccountBalance)
 	}
 
 	if err := k.SetParams(ctx, genState.Params); err != nil {
@@ -29,5 +31,6 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	}
 	genesis := types.DefaultGenesis()
 	genesis.Params = params
+	genesis.ModuleAccountBalance = k.GetModuleAccountBalance(ctx)
 	return genesis
 }
