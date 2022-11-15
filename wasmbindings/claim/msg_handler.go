446b5b83c6f938da8fb2af350204ae669bfaab15
geekypanda25 <saishlok@hotmail.com>
2022-11-15 09:51:19 -0700

    revert imports back

--- wasmbindings/claim/msg_handler.go
+++ wasmbindings/claim/msg_handler.go
@@ -7,7 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	"wasmbindings/claims/types"
+	"github.com/envadiv/Passage3D/wasmbindings/claims/types"
 	claimTypes "github.com/envadiv/Passage3D/x/claim/types"
 )
 
