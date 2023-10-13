57123a155e4a7ec424a12430a0c529eb378966c5
atheesh <atheesh@vitwit.com>
2023-10-13 11:02:49 +0530

    review changes

--- app/upgrades/v2.2.0/upgrade.go
+++ app/upgrades/v2.2.0/upgrade.go
@@ -7,6 +7,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	auth "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	distribution "github.com/cosmos/cosmos-sdk/x/distribution/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -16,6 +18,10 @@ import (
 )
 
 const Name = "v2.2.0"
+const upasgDenom = "upasg"
+
+// 150,000,000 $PASG tokens
+var amount = sdk.NewCoins(sdk.NewCoin(upasgDenom, sdk.NewInt(150000000000000)))
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          Name,
@@ -26,14 +32,14 @@ var Upgrade = upgrades.Upgrade{
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	_ distribution.Keeper,
+	dk distribution.Keeper,
 	bk bank.Keeper,
 	ak auth.AccountKeeper,
 	ck claim.Keeper,
 ) upgradetypes.UpgradeHandler {
 
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		if err := ExecuteProposal(ctx, ak, bk, ck); err != nil {
+		if err := ExecuteProposal(ctx, ak, bk, ck, dk); err != nil {
 			return nil, err
 		}
 
@@ -41,17 +47,33 @@ func CreateUpgradeHandler(
 	}
 }
 
-func ExecuteProposal(ctx sdk.Context, ak auth.AccountKeeper, bk bank.Keeper, ck claim.Keeper) error {
+func ExecuteProposal(ctx sdk.Context, ak auth.AccountKeeper, bk bank.Keeper, ck claim.Keeper, dk distribution.Keeper) error {
 	var sixMonths = time.Hour * 24 * 180
-	newClaimRecords := getClaimRecords()
+
+	vestingAcc, err := sdk.AccAddressFromBech32("pasg105488mw9t3qtp62jhllde28v40xqxpjksjqmvx")
+	if err != nil {
+		return err
+	}
+
+	// 3 year lock-up from relaunch and thereafter weekly vesting until end of year 5 from relaunch
+	pva := vestingtypes.NewPeriodicVestingAccount(authtypes.NewBaseAccount(vestingAcc, nil, ak.GetNextAccountNumber(ctx), 0),
+		amount,
+		1784905200,
+		genVestingPeriods(),
+	)
+	ak.SetAccount(ctx, pva)
+
+	if err := dk.DistributeFromFeePool(ctx, amount, vestingAcc); err != nil {
+		return err
+	}
 
 	// sum the newly added claim records balance
 	var amount sdk.Coins
-	for _, record := range newClaimRecords {
+	for _, record := range NewClaimRecords {
 		amount.Add(record.ClaimableAmount...)
 
 		// set the claim record in claim module
-		ck.SetClaimRecord(ctx, record)
+		ck.SetClaimRecord(ctx, *record)
 	}
 
 	// get airdrop account
@@ -73,4 +95,21 @@ func ExecuteProposal(ctx sdk.Context, ak auth.AccountKeeper, bk bank.Keeper, ck 
 
 	ck.SetParams(ctx, params)
 	return nil
+}
+
+func genVestingPeriods() []vestingtypes.Period {
+	var periods []vestingtypes.Period
+	periods = append(periods, vestingtypes.Period{
+		Length: 0,
+		Amount: sdk.NewCoins(sdk.NewCoin(upasgDenom, sdk.NewInt(1442307692379))),
+	})
+
+	for i := 0; i < 103; i++ {
+		periods = append(periods, vestingtypes.Period{
+			Length: 604800,
+			Amount: sdk.NewCoins(sdk.NewCoin(upasgDenom, sdk.NewInt(1442307692307))),
+		})
+	}
+
+	return periods
 }
